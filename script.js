de72ab5517f1524197137445052511683da84b8d
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2024-02-15 22:05:36 +0530

    feat : fullscreen, theater, miniplayer modes

--- script.js
+++ script.js
@@ -1,19 +1,73 @@
-const playPauseButton = document.querySelector(".play-pause-btn");
+const playPauseBtn = document.querySelector(".play-pause-btn");
+const theaterBtn = document.querySelector(".theater-btn");
+const fullScreenBtn = document.querySelector(".full-screen-btn");
+const miniPlayerBtn = document.querySelector(".mini-player-btn");
 const videoContainer = document.querySelector(".video-container");
 const video = document.querySelector("video");
 
 document.addEventListener("keydown", (e) => {
+  const tagName = document.activeElement.tagName.toLowerCase();
+
+  if (tagName === "input") return;
   switch (e.key.toLowerCase()) {
     case " ":
+      if (tagName === "button") return;
     case "k":
       togglePlay();
+      break;
+    case "f":
+      toggleFullScreenMode();
+      break;
+    case "t":
+      toggleTheaterMode();
+      break;
+    case "i":
+      toggleMiniPlayerMode();
       break;
   }
 });
 
-// Play/Pause
+//View Modes
+theaterBtn.addEventListener("click", toggleTheaterMode);
 
-playPauseButton.addEventListener("click", togglePlay);
+fullScreenBtn.addEventListener("click", toggleFullScreenMode);
+
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode);
+
+function toggleTheaterMode() {
+  videoContainer.classList.toggle("theater");
+}
+
+function toggleFullScreenMode() {
+  if (document.fullscreenElement == null) {
+    videoContainer.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+function toggleMiniPlayerMode() {
+  if (videoContainer.classList.contains("mini-player")) {
+    document.exitPictureInPicture();
+  } else {
+    video.requestPictureInPicture();
+  }
+}
+
+document.addEventListener("fullscreenchange", () => {
+  videoContainer.classList.toggle("full-screen", document.fullscreenElement);
+});
+
+video.addEventListener("enterpictureinpicture", () => {
+  videoContainer.classList.add("mini-player");
+});
+
+video.addEventListener("leavepictureinpicture", () => {
+  videoContainer.classList.remove("mini-player");
+});
+
+// Play/Pause
+playPauseBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 
 function togglePlay() {
